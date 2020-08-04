7ebe3749c27e204efc52c13556e247a0428173de
Mauricio Junior <mauricio.silva@vermont.com.br>
2020-08-04 13:44:09 -0300

    Broke unit test

--- src/app/peoples-list/peoples-list.component.spec.ts
+++ src/app/peoples-list/peoples-list.component.spec.ts
@@ -1,29 +1,34 @@
-import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
-import { Observable } from 'rxjs';
-import { PeoplesListComponent } from './peoples-list.component';
-import { PeoplesListService } from './peoples-list.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { By } from '@angular/platform-browser';
-import { PersonItemComponent } from './person-item/person-item.component';
-import { MatCardModule } from '@angular/material/card';
-import { MockComponent } from 'ng-mocks';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from "@angular/core/testing";
+import { Observable } from "rxjs";
+import { PeoplesListComponent } from "./peoples-list.component";
+import { PeoplesListService } from "./peoples-list.service";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { By } from "@angular/platform-browser";
+import { PersonItemComponent } from "./person-item/person-item.component";
+import { MatCardModule } from "@angular/material/card";
+import { MockComponent } from "ng-mocks";
 
 export class DataStub {
   public getPeoples(): Observable<any> {
     return new Observable((observer) => {
-      observer.next([{ name: 'Joao 1' }, { name: 'Jose 2' }]);
+      observer.next([{ name: "Joao 1" }, { name: "Jose 2" }]);
     });
   }
 }
 
-describe('PeoplesListComponent', () => {
+describe("PeoplesListComponent", () => {
   let component: PeoplesListComponent;
   let fixture: ComponentFixture<PeoplesListComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PeoplesListComponent, MockComponent(PersonItemComponent) ],
+      declarations: [PeoplesListComponent, MockComponent(PersonItemComponent)],
       providers: [{ provide: PeoplesListService, useClass: DataStub }],
     });
 
@@ -32,8 +37,8 @@ describe('PeoplesListComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should list people names', () => {
-    const li = fixture.debugElement.queryAll(By.css('app-person-item'));
-    expect(li.length).toEqual(2);
+  it("should list people names", () => {
+    const li = fixture.debugElement.queryAll(By.css("app-person-item"));
+    expect(li.length).toEqual(3);
   });
 });
